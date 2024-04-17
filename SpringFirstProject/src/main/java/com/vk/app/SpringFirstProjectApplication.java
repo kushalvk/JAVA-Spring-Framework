18ead2f0010e65f5c01f1503a56547620d7c35a6
kushal <kushalvaghela2003@gmail.com>
2024-04-17 11:44:06 +0530

    First

---
+++ SpringFirstProject/src/main/java/com/vk/app/SpringFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.vk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstProjectApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
