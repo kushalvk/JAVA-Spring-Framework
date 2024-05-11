d9048f11fbf11eb6fceb60a5691f77e157405b29
kushal <kushalvaghela2003@gmail.com>
2024-05-11 12:44:42 +0530

    API Getway

---
+++ api-getway-20/src/main/java/com/vk/api/getway0/ApiGetway20Application.java
@@ -0,0 +1,13 @@
+package com.vk.api.getway0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetway20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetway20Application.class, args);
+	}
+
+}
---
+++ api-getway-20/src/test/java/com/vk/api/getway0/ApiGetway20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.api.getway0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetway20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
