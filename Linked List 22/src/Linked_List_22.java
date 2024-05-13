af625f63df6f0401ecf4e1fe0bfa847bc5ad1307
kushal <kushalvaghela2003@gmail.com>
2024-05-13 11:13:07 +0530

    Link List

---
+++ Linked List 22/src/Linked_List_22.java
@@ -0,0 +1,18 @@
+
+public class Linked_List_22 {
+    public static void main(String[] args) {
+
+        LinkedList nums = new LinkedList();
+
+        nums.add(5);
+        nums.add(9);
+        nums.add(6);
+
+        nums.addFirst(7);
+
+        nums.delete(9);
+
+        nums.prientValues();
+
+    }
+}
