bf24786a679fa86da49daec1ccbd5758faf68f7b
kushal <kushalvaghela2003@gmail.com>
2024-05-15 18:15:16 +0530

    Bookmark application

--- Bookmark-app-26/src/main/webapp/script.js
+++ Bookmark-app-26/src/main/webapp/script.js
@@ -1,7 +1,7 @@
 function save() {
    let input = document.querySelector("#in").value;
    let output = document.querySelector("#out");
-   output.innerHTML += `<a href="#"> ${input} </a><br>`;
+   output.innerHTML += `<a href="${input}"> ${input} </a><br>`;
 }
 function remove() {
    let output = document.querySelector("#out");
