af4e93dad5fc5fd2aecd60fbb7acc9c117c0ff42
Kushal Vaghela <kushalvaghela2003@gmail.com>
2024-06-20 10:45:10 +0530

    Job Management System

--- Job-Management-System/src/main/java/com/vk/Job/Management/System/Service/JwtService.java
+++ Job-Management-System/src/main/java/com/vk/Job/Management/System/Service/JwtService.java
@@ -45,7 +45,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*1))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60))
                 .signWith(getKey(), SignatureAlgorithm.HS256).compact();
     }
 
